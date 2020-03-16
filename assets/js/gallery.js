de63b0086666f644375222a778da470a7e013afa
themaleem <themaleem@gmail.com>
2020-03-16 21:52:11 +0100

    minifies bg-images and added gallery section

---
+++ assets/js/gallery.js
@@ -0,0 +1,35 @@
+!(function($) {
+  "use strict";
+
+  // Initialize Venobox
+  $('.venobox').venobox({
+    bgcolor: '',
+    overlayColor: 'rgba(6, 12, 34, 0.85)',
+    closeBackground: '',
+    closeColor: '#fff'
+  });
+
+
+  // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
+  });
+
+})(jQuery);
